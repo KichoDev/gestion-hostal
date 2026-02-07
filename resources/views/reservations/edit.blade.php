932d34c692cac130d5f693cfea1ecd6d53ec7e45
Mauricio Santis <msantisdev@gmail.com>
2026-02-07 00:58:33 -0300

    Revert "Creación de controlador y vistas de reserva"
    This reverts commit df12cde70507ecac650c5b4063e5e2cc25c78817.

--- resources/views/reservations/edit.blade.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="{{ route('reservations.update', $reservation) }}">
-    @csrf
-    @method('PUT')
-    @include('reservations.form', ['reservation' => $reservation])
-    <x-primary-button>Actualizar Reserva</x-primary-button>
-</form>
